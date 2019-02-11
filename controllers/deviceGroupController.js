a2463da2109bbd769431b50e0ab18f5ac7c8968e
cfarrell <conorf50@gmail.com>
2019-02-11 21:06:15 +0000

    added new files, improved logging

--- controllers/deviceGroupController.js
+++ controllers/deviceGroupController.js
@@ -6,11 +6,17 @@ var DAO = require('../lib/dao');
 var moment = require('moment');
 function addDeviceGroup(req, res) {
     // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-    var input = req.swagger.params
-    var groupName = input.undefined.value.groupName
-    // console.log(input.undefined.value.timestamp)
+    let input = req.swagger.params
+    let groupName = input.undefined.value.groupName
+    let groupLatitude = input.undefined.value.groupLat
+    let groupLongitude = input.undefined.value.groupLong
+    console.log(groupLatitude)
 
-    res.json("Group: '" + groupName + "' created");
+
+    // these are template strings
+    // see here: https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+    res.json(`Group: '${groupName}' created with lat ${groupLatitude} and long ${groupLongitude}`);
+
   }
   
   module.exports = {
