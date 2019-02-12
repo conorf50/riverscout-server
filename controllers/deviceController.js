b23871724b3d6bff856f3d505f6454865c554eee
cfarrell <conorf50@gmail.com>
2019-02-12 11:27:50 +0000

    import issue fixed

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -6,10 +6,10 @@ var deviceDAO = require('../lib/deviceDAO');
 var moment = require('moment');
 function addNewDevice(req, res) {
     // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-    var input = req.swagger.params
-    console.log(input.undefined.value)
+    var input = req.swagger.params.undefined.value
+    console.log("displayName" + input.displayName)
     // console.log(input.undefined.value.timestamp)
-    deviceDAO.saveDeviceData(input.undefined.value)
+    deviceDAO.saveDeviceData(input)
     res.json("Data added to database");
   }
   
--- schemas/deviceSchema.js
+++ schemas/deviceSchema.js
@@ -13,7 +13,7 @@ var Schema = mongoose.Schema;
 // This collection contains info for a device like it's install data
 // and maintainence schedules. There is one of these per device.
 const DeviceSchema = new Schema({
-    _id: mongoose.Schema.Types.ObjectId(), // use this as a unique ID for a gauge even if it has been replaced. This is referenced in 'sigfoxDataSchema.js"
+    //_id: mongoose.Schema.Types.ObjectId(), // use this as a unique ID for a gauge even if it has been replaced. This is referenced in 'sigfoxDataSchema.js"
     displayName: String,                 // name to display on front end. Eg: "Suir at Kilsheelan Bridge"
     gpsLat: mongoose.Types.Decimal128, // see http://thecodebarbarian.com/a-nodejs-perspective-on-mongodb-34-decimal.html
     gpsLong: mongoose.Types.Decimal128, // and https://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude/8674#8674 
@@ -25,7 +25,7 @@ const DeviceSchema = new Schema({
 
     reportingFreq: Number, // how often (in minutes) the device is expected to send up data
     downlinkEnabled: Boolean, // can the device recieve data
-    group : {type: mongoose.Schema.Types.ObjectId(), ref: 'groupModel', required = true}, // reference the 'groupModel' in 'deviceGroups.js'
+    group : {type: mongoose.Types.ObjectId, ref: 'groupModel', required : true}, // reference the 'groupModel' in 'deviceGroups.js'
     activeStatus: Boolean, // is the device currently active?
     deviceHistory: {     // array containing all the history associated with a device. Empty on creation, not specified through add device API
       timestamp: Date,
