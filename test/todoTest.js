9929b939edfa77b132b92cee0b67fc1792e033b4
cfarrell <conorf50@gmail.com>
2019-02-20 19:44:04 +0000

    test structure implemented

---
+++ test/todoTest.js
@@ -0,0 +1,17 @@
+/*
+    Coda from the following tutorial : https://www.codementor.io/olatundegaruba/integration-testing-supertest-mocha-chai-6zbh6sefz
+*/
+
+'use strict';
+
+var app = require('../app'),
+  chai = require('chai'),
+  request = require('supertest');
+
+var expect = chai.expect;
+
+
+
+describe('Todos list API Integration Tests', function() {
+
+});
