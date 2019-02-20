24bc723003c092801e5b1a0505fa3918a9ea805b
cfarrell <conorf50@gmail.com>
2019-02-20 19:27:51 +0000

    added conf files

---
+++ config/development.js
@@ -0,0 +1,16 @@
+/*
+Below code adapted from the tutorial here
+https://www.codementor.io/olatundegaruba/integration-testing-supertest-mocha-chai-6zbh6sefz
+
+
+As mentioned in the tutorial, this is a configuration file
+*/
+
+
+'use strict';
+
+module.exports = {
+  env: 'development',
+  db: 'mongodb://riverscout:riverscout@10.10.1.10/riverscout',
+  port: process.env.PORT || 3000,
+};
