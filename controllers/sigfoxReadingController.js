b8b6b6e617138799933cee415015a10278e36c90
cfarrell <conorf50@gmail.com>
2019-02-23 20:00:48 +0000

    rename api route

---
+++ controllers/sigfoxReadingController.js
@@ -0,0 +1,37 @@
+'use strict'
+var DAO = require('../lib/dao');
+
+// date library that allows relative dates like .fromNow, subtract X days and more
+var moment = require('moment');
+function addSigfoxReading(req, res) {
+    // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+    var input = req.swagger.params
+    // console.log(input)
+    // console.log(input.undefined.value.timestamp)
+
+  
+     var device = input.undefined.value.device
+     var data = input.undefined.value.data
+     var tsString = input.undefined.value.timestamp
+     var tsInt = parseInt(tsString, 10) // specify a base of 10 as the second arg
+  
+     var momentTs  = new moment.unix(tsInt); // important since we are recieving UTC unix timstamps from Sigfox
+     console.log("integer timestamp = " + tsInt)
+     console.log("converted moment = " + momentTs)
+    // // save the data
+    DAO.saveDeviceData(device, momentTs, data)
+    .then(function(x){
+      res.json(x);
+    })
+    // .catch(function(err){
+    //   res.json(err)
+    // })
+    
+    // this sends back a JSON response which is a single string
+    // res.json("Data added to database");
+  }
+  
+  module.exports = {
+      addSigfoxReading: addSigfoxReading
+    };
+  
