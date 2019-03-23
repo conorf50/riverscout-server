74a757dcf553096acc7a603e92b5dd3170763368
cfarrell <conorf50@gmail.com>
2019-03-23 12:43:31 +0000

    removed mocha opts added setup and teardown hooks

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,7 @@
 
+// created with help from here:
+//https://stackoverflow.com/questions/10561598/global-before-and-beforeeach-for-mocha
+
 before(function(){
     console.log("This is a setup hook!")
 })
---
+++ test/teardown.js
@@ -0,0 +1,9 @@
+// created with help from here:
+//https://stackoverflow.com/questions/10561598/global-before-and-beforeeach-for-mocha
+
+after(function(){
+    console.log("Tearing down test suite")
+})
+
+// add mockgoose stuff in here
+
