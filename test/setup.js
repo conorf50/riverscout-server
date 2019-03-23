5737d7b384c263d1e1c8a151b3ff902884226201
cfarrell <conorf50@gmail.com>
2019-03-23 14:50:11 +0000

    remove config dir

--- test/setup.js
+++ test/setup.js
@@ -2,10 +2,10 @@
 // created with help from here:
 //https://stackoverflow.com/questions/10561598/global-before-and-beforeeach-for-mocha
 
+
 before(function(){
     console.log("This is a setup hook!")
+    // return dbsetup
 })
-
-// add mockgoose stuff in here
 
 
