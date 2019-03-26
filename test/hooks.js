0493fd0e68ec95238daadcdcaab8be5ee792e433
cfarrell <conorf50@gmail.com>
2019-03-26 15:35:09 +0000

    test file calls DAO function

--- test/hooks.js
+++ test/hooks.js
@@ -2,7 +2,7 @@
 // created with help from here:
 //https://stackoverflow.com/questions/50546533/unit-test-node-js-mongoose-mocha-chai-sinon
 var mongoose = require('mongoose');
-var CountrySchema = require('../schemas/countrySchema')
+var CountryDAO = require('../lib/countryDAO')
 mongoose.set('debug', true);
 
 /*
@@ -16,7 +16,7 @@ db.createUser({
 */
 
 
-before(function () {
+before (function () {
     console.log("This is a setup hook!")
     mongoose.connect('mongodb://npm:unittesting@10.10.1.10/npm_test_db', { useNewUrlParser: true }); // set up the connection with the above IP address ,user and password  
     // Get Mongoose to use the global promise library
@@ -28,7 +28,15 @@ before(function () {
     
     //Bind connection to error event (to get notification of connection errors)
     db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-})
+    
+    return CountryDAO.createOrUpdateCountry("Ireland", "IE")
+
+    // CountrySchema.insertOne([{
+    //     countryName: "Ireland ",                 // name to display on front end. Eg: "Ireland" , "United Kingdom" etc...
+    //     code: "IE",  // eg: 'IE', 'UK', 'DE' etc.....
+    //     countryID: new mongoose.Types.ObjectId
+    // }]);
+});
 
 after(function () {
     console.log("Tearing down test suite")
