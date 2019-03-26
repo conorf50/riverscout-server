2d52041607f018130311997c50655adf61c45d9f
cfarrell <conorf50@gmail.com>
2019-03-26 15:01:59 +0000

    add catch to DAO functions

--- test/hooks.js
+++ test/hooks.js
@@ -5,21 +5,32 @@ var mongoose = require('mongoose');
 var CountrySchema = require('../schemas/countrySchema')
 mongoose.set('debug', true);
 
+/*
+Test db details.
+
+db.createUser({
+    user: 'npm',
+    pwd: 'unittesting',
+    roles: [{ role: 'readWrite', db:'npm_test_db'}]
+})
+*/
 
 
-before(function(){
-console.log("This is a setup hook!")
-mongoose.connect('mongodb://riverscout:riverscout@10.10.1.10/riverscout',{ useNewUrlParser: true }); // set up the connection with the above IP address ,user and password  
-// Get Mongoose to use the global promise library
-mongoose.Promise = global.Promise;
-//Get the default connection
-var db = mongoose.connection;
+before(function () {
+    console.log("This is a setup hook!")
+    mongoose.connect('mongodb://npm:unittesting@10.10.1.10/npm_test_db', { useNewUrlParser: true }); // set up the connection with the above IP address ,user and password  
+    // Get Mongoose to use the global promise library
+    mongoose.Promise = global.Promise;
+    //Get the default connection
+    var db = mongoose.connection;
+    // insert some sample data into the database
 
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+    
+    //Bind connection to error event (to get notification of connection errors)
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 })
 
-after(function(){
+after(function () {
     console.log("Tearing down test suite")
 })
 
