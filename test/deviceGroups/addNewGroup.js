3b784989f78d32995d92862c13e4a888ef778596
cfarrell <conorf50@gmail.com>
2019-04-05 20:53:51 +0000

    basic POST request working

--- test/deviceGroups/addNewGroup.js
+++ test/deviceGroups/addNewGroup.js
@@ -10,32 +10,17 @@ var app = require('../../app'),
 var expect = chai.expect;
 var util = require('util') // import for the util.inspect method
 
-
-    describe('#GET / deviceGroups', function() { 
-      it('should get all device groups for the country code "IE"', function(done) { 
-        request(app) .get('/api/getAllDeviceGroups?countryCode=IE')
+    describe('#POST / deviceGroups', function() { 
+      it('should add a new group country code "IE"', function(done) { 
+        request(app) .post('/api/addOrUpdateDeviceGroup')
+        .send({
+            "groupLat":64.2341,
+            "groupLong": -7.9922,
+            "groupName": "New Group",
+            "countryCode": "IE"
+        })
           .end(function(err, res) { 
-            expect(res.statusCode).to.equal(200); 
-            // check the names of each group
-            expect(res.body[0].groupName).to.equal("Unit Test Group #1");
-            expect(res.body[1].groupName).to.equal("Unit Test Group #4");
-            expect(res.body[2].groupName).to.equal("Unit Test Group #5");
-
-            //check if all the countrycodes match
-            expect(res.body[0].countryCode).to.equal("IE");
-            expect(res.body[1].countryCode).to.equal("IE");
-            expect(res.body[2].countryCode).to.equal("IE");
-
-            // check the locations of each of the groups
-            expect(res.body[0].groupLat.$numberDecimal).to.equal("52.4542");
-            expect(res.body[0].groupLong.$numberDecimal).to.equal("-7.3453");
-
-            expect(res.body[1].groupLat.$numberDecimal).to.equal("53.9932");
-            expect(res.body[1].groupLong.$numberDecimal).to.equal("-7.6453");
-
-            expect(res.body[2].groupLat.$numberDecimal).to.equal("51.4541");
-            expect(res.body[2].groupLong.$numberDecimal).to.equal("-6.9989");
-
+           
             //console.log("RES = " + util.inspect(res.body))
             done(); 
           }); 
