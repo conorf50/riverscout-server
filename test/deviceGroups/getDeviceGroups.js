3880e680ade8935477a3c06c30488f2352b65fbf
cfarrell <conorf50@gmail.com>
2019-04-05 19:46:07 +0000

    added more tests

--- test/deviceGroups/getDeviceGroups.js
+++ test/deviceGroups/getDeviceGroups.js
@@ -11,7 +11,7 @@ var expect = chai.expect;
 var util = require('util') // import for the util.inspect method
 
 
-describe('Get All Device Groups for code "IE" ', function() {
+describe('Get All Device Groups for country code "IE" ', function() {
     describe('#GET / deviceGroups', function() { 
       it('should get all device groups for the country code "IE"', function(done) { 
         request(app) .get('/api/getAllDeviceGroups?countryCode=IE')
@@ -37,10 +37,61 @@ describe('Get All Device Groups for code "IE" ', function() {
             expect(res.body[2].groupLat.$numberDecimal).to.equal("51.4541");
             expect(res.body[2].groupLong.$numberDecimal).to.equal("-6.9989");
 
-            console.log("RES = " + util.inspect(res.body))
+            //console.log("RES = " + util.inspect(res.body))
             done(); 
           }); 
       });
     });
   });
 
+  describe('Get All Device Groups for country code "UK" ', function() {
+    describe('#GET / deviceGroups', function() { 
+      it('should get all device groups for the country code "UK"', function(done) { 
+        request(app) .get('/api/getAllDeviceGroups?countryCode=UK')
+          .end(function(err, res) { 
+            expect(res.statusCode).to.equal(200); 
+            // check the names of each group
+            expect(res.body[0].groupName).to.equal("Unit Test Group #2");
+            expect(res.body[1].groupName).to.equal("Unit Test Group #6");
+
+            //check if all the countrycodes match
+            expect(res.body[0].countryCode).to.equal("UK");
+            expect(res.body[1].countryCode).to.equal("UK");
+
+            // check the locations of each of the groups
+            expect(res.body[0].groupLat.$numberDecimal).to.equal("69.6969");
+            expect(res.body[0].groupLong.$numberDecimal).to.equal("-4.2002");
+
+            expect(res.body[1].groupLat.$numberDecimal).to.equal("57.4322");
+            expect(res.body[1].groupLong.$numberDecimal).to.equal("-9.1002");
+
+            //console.log("RES for UK= " + util.inspect(res.body))
+            done(); 
+          }); 
+      });
+    });
+  });
+
+
+  describe('Get All Device Groups for country code "FR" ', function() {
+    describe('#GET / deviceGroups', function() { 
+      it('should get all device groups for the country code "FR"', function(done) { 
+        request(app) .get('/api/getAllDeviceGroups?countryCode=FR')
+          .end(function(err, res) { 
+            expect(res.statusCode).to.equal(200); 
+            // check the names of each group
+            expect(res.body[0].groupName).to.equal("Unit Test Group #3");
+
+
+            //check if all the countrycodes match
+            expect(res.body[0].countryCode).to.equal("FR");
+
+            // check the locations of each of the groups
+            expect(res.body[0].groupLat.$numberDecimal).to.equal("45.5232");
+            expect(res.body[0].groupLong.$numberDecimal).to.equal("1.3453");
+
+            done(); 
+          }); 
+      });
+    });
+  });
