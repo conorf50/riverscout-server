4f687bfd6fa5291569830738d6ace9481e519e10
cfarrell <conorf50@gmail.com>
2019-04-15 13:17:39 +0000

    added sigfox helper

--- test/hooks.js
+++ test/hooks.js
@@ -1,29 +1,31 @@
 
 // created with help from here:
 //https://stackoverflow.com/questions/50546533/unit-test-node-js-mongoose-mocha-chai-sinon
+// Most of this file is from the above source
 const mongoose = require('mongoose');
 
-// require the helper files
-const countryHelper = require('./helpers/countryHelper');
-const groupHelper = require('./helpers/groupHelper');
+// require the helper files - makes the code cleaner
+const countryHelper = require('./helpers/countryHelper'); // own work
+const groupHelper = require('./helpers/groupHelper');  // own work
 
-// enable Mongoose debugging
+// enable Mongoose debugging (from Mozilla tutorial)
 mongoose.set('debug', true);
 
 // this will run before the tests.
-before(async function () {
+before(async function () { 
+    // use async await to make things predictable
     console.log("Clearing existing data and starting tests.")
-    // start with a clean slate
+    // start with a clean slate by clearing any existing data in the test
     await countryHelper.purge()
     await groupHelper.purge()
-    // insert some sample data into the database
+    // insert some sample data into the database (see the JSON files in the 'files' directory)
     await countryHelper.populate()
     await groupHelper.populate()
 });
 
 after(async function () {
     // clear the database on test exit
-    // console.log("Tearing down test suite")
+    console.log("Tearing down test suite and purging database")
     await countryHelper.purge()
     await groupHelper.purge()
 })
