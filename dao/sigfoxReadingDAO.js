1b3c16a887e5e4eb744e49fb044f213cd88356fd
cfarrell <conorf50@gmail.com>
2019-06-21 22:01:50 +0000

    added new files

--- dao/sigfoxReadingDAO.js
+++ dao/sigfoxReadingDAO.js
@@ -11,6 +11,7 @@ db.createUser({
 */
 
 var mongoose = require('mongoose');
+var moment = require('moment')
 // import all of the dependencies (schemas)
 var sigfoxDataSchema = require('../schemas/sigfoxDataSchema')
 
@@ -24,73 +25,93 @@ var sigfoxDAO = {} // do this so we can use the same name for module.exports whi
 // new functions will be accessed by caling DAO.<newFunc>
 
 // sigfoxID, momentTs, rawHexString
-sigfoxDAO.saveDeviceData = function(sigfoxID,momentTs, rawHexData, waterLevel, waterTemp) {
+sigfoxDAO.saveDeviceData = function (sigfoxID, momentTs, rawHexData, waterLevel, waterTemp) {
     return new sigfoxDataSchema.sigfox_device_measurement({
-                deviceUID : sigfoxID, // todo fix this
-                rawHexData : rawHexData,
-                waterLevel: waterLevel,
-                waterTemp: waterTemp,
-                timestamp : momentTs
-            })
-    .save()
-    .then(function(y) {
-        return y
+        deviceUID: sigfoxID, // todo fix this
+        rawHexData: rawHexData,
+        waterLevel: waterLevel,
+        waterTemp: waterTemp,
+        timestamp: momentTs
     })
-    .catch(function(err) {
-        return err
-    })
+        .save()
+        .then(function (y) {
+            return y
+        })
+        .catch(function (err) {
+            return err
+        })
 }
 
 
-sigfoxDAO.getDeviceData = function(deviceUID, timestampGt, timestampLt) {
+sigfoxDAO.getDeviceData = function (deviceUID, timestampGt, timestampLt) {
 
     // find measurements for the selected device between 'timestampGt' and 'timestampLt'
-    
-return sigfoxDataSchema.sigfox_device_measurement.find({
-                deviceUID : deviceUID,
-                "timestamp":{
-                    $gt: timestampGt, // greater than this date
-                    $lt: timestampLt // less than this date
+
+    // return sigfoxDataSchema.sigfox_device_measurement.find({
+    //                 deviceUID : deviceUID,
+    // "timestamp":{
+    //     $gt: timestampGt, // greater than this date
+    //     $lt: timestampLt // less than this date
+    // }
+    //             })
+    //     .then(function(data) {
+    //         console.log(data)
+    //         return data ;
+    //     })
+    //     .catch(err => {
+    //         return err
+    //     })
+
+
+
+    return sigfoxDataSchema.sigfox_device_measurement.aggregate([{
+        $match: {
+            $and: [
+                { "deviceUID": deviceUID },
+                {
+                    "timestamp": {
+                        $gt: Date(timestampGt),
+                        $lt: Date(timestampLt)
+                    }
                 }
-            })
-    .then(function(data) {
-        console.log(data)
-        return data ;
-    })
-    .catch(err => {
-        return err
-    })
+            ]
+        }
+    }])
+
+
+
+
 }
 
 
-sigfoxDAO.deleteOneReading = function(readingID){
+sigfoxDAO.deleteOneReading = function (readingID) {
     // delete one reading matching the ID specified
     return sigfoxDataSchema.sigfox_device_measurement.deleteOne({
         _id: readingID
     })
-    .then(function(z){
-        return z
-    })
-    .catch(function(err){
-        return(
-            err.message
-        )
-    })
+        .then(function (z) {
+            return z
+        })
+        .catch(function (err) {
+            return (
+                err.message
+            )
+        })
 }
 
-sigfoxDAO.deleteAllReadings = function(deviceID){
-    
+sigfoxDAO.deleteAllReadings = function (deviceID) {
+
     return sigfoxDataSchema.sigfox_device_measurement.deleteMany({
         deviceUID: deviceID
     })
-    .then(function(z){
-        return z
-    })
-    .catch(function(err){
-        return(
-            err.message
-        )
-    })
+        .then(function (z) {
+            return z
+        })
+        .catch(function (err) {
+            return (
+                err.message
+            )
+        })
 }
 
 module.exports = sigfoxDAO;
